724b104e5695a4d8bdbc4562217715cad0fadaff
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2024-06-26 12:50:12 -0400

    feat: Getting all the folder names from the shared drive

--- script.py
+++ script.py
@@ -1,83 +1,54 @@
-# import the required libraries 
-import pickle 
-import os.path 
-from googleapiclient.discovery import build 
-from google_auth_oauthlib.flow import InstalledAppFlow 
-from google.auth.transport.requests import Request 
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
+SCOPES = ['https://www.googleapis.com/auth/drive']
+SHARED_DRIVE_ID = '0AGsI-FsgSjnKUk9PVA'
 
+def get_folder_list():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
-# Define the SCOPES. If modifying it, 
-# delete the token.pickle file. 
-SCOPES = ['https://www.googleapis.com/auth/drive'] 
+    service = build('drive', 'v3', credentials=creds)
 
+    folders = []
+    page_token = None
+    
+    while True:
+        results = service.files().list(
+            q=f"mimeType='application/vnd.google-apps.folder' and '{SHARED_DRIVE_ID}' in parents",
+            supportsAllDrives=True,
+            includeItemsFromAllDrives=True,
+            driveId=SHARED_DRIVE_ID,
+            corpora='drive',
+            fields="nextPageToken, files(id, name)",
+            pageToken=page_token,
+            pageSize=1000
+        ).execute()
 
+        folders.extend(results.get('files', []))
+        page_token = results.get('nextPageToken', None)
+        if page_token is None:
+            break
 
-# Create a function getFileList with 
-# parameter N which is the length of 
-# the list of files. 
-def getFileList(N): 
+    return folders
 
-	# Variable creds will store the user access token. 
-	# If no valid token found, we will create one. 
-	creds = None
+folder_list = get_folder_list()
 
-	# The file token.pickle stores the 
-	# user's access and refresh tokens. It is 
-	# created automatically when the authorization 
-	# flow completes for the first time. 
-
-	# Check if file token.pickle exists 
-	if os.path.exists('token.pickle'): 
-
-		# Read the token from the file and 
-		# store it in the variable creds 
-		with open('token.pickle', 'rb') as token: 
-			creds = pickle.load(token) 
-
-	# If no valid credentials are available, 
-	# request the user to log in. 
-	if not creds or not creds.valid: 
-
-		# If token is expired, it will be refreshed, 
-		# else, we will request a new one. 
-		if creds and creds.expired and creds.refresh_token: 
-			creds.refresh(Request()) 
-		else: 
-			flow = InstalledAppFlow.from_client_secrets_file( 
-				'credentials.json', SCOPES) 
-			creds = flow.run_local_server(port=0) 
-
-		# Save the access token in token.pickle 
-		# file for future usage 
-		with open('token.pickle', 'wb') as token: 
-			pickle.dump(creds, token) 
-
-	# Connect to the API service 
-	service = build('drive', 'v3', credentials=creds) 
-
-	# request a list of first N files or 
-	# folders with name and id from the API. 
-	resource = service.files() 
-	result = resource.list(pageSize=N, fields="files(id, name)").execute() 
-
-	# return the result dictionary containing 
-	# the information about the files 
-	return result 
-
-
-	
-# Get list of first 5 files or 
-# folders from our Google Drive Storage 
-result_dict = getFileList(5) 
-
-
-
-# Extract the list from the dictionary 
-file_list = result_dict.get('files') 
-
-
-
-# Print every file's name 
-for file in file_list: 
-	print(file['name']) 
+print(f"Total folders found: {len(folder_list)}")
+for folder in folder_list:
+    print(f"Folder Name: {folder['name']}, Folder ID: {folder['id']}")
